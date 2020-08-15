9e41ee7d27b63bad81b5c595d8f143cc76206dda
Kun Lin <kun.lin.596@gmail.com>
2020-08-15 17:33:16 +0900

    (fix) Minor cleanup

--- python/camera_calibration.py
+++ python/camera_calibration.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 import os
-import cv2, glob
+import cv2
+import glob
 import numpy as np
+import matplotlib.pyplot as plt
+from IPython import embed
 
-import scipy
-from scipy import spatial
-from scipy.spatial import transform
 from scipy.spatial.transform import Rotation as R
 
 import logging
@@ -24,18 +24,16 @@ except ImportError as e:
 import matplotlib
 matplotlib.use('TkAgg')
 
-import matplotlib.pyplot as plt
-
-from IPython import embed
-
 N_ROWS = 6
 N_COLS = 9
+
 
 def _generate_object_points(nImages, nRows=N_ROWS, nCols=N_COLS):
     objectPointsPerImage = np.zeros(shape=(nRows * nCols, 3), dtype=np.float32)
     objectPointsPerImage[:, :2] = np.mgrid[0:nRows, 0:nCols].T.reshape(-1, 2)
     objPoints = np.tile(objectPointsPerImage, reps=(nImages, 1)).reshape(nImages, nRows * nCols, 3)
     return objPoints
+
 
 def load_images(imagePath):
     if not os.path.exists(imagePath):
@@ -53,6 +51,7 @@ def load_images(imagePath):
 
     return images
 
+
 def detect_chess_board_corners(images, show=False):
     imagePoints = {}
     nImages = len(images)
@@ -60,11 +59,11 @@ def detect_chess_board_corners(images, show=False):
     if show:
         nCols = 5
         nRows = int(np.round(nImages * 1.0 / nCols))
-        axes = plt.subplots(nRows, nCols)
+        plt.subplots(nRows, nCols)
         plt.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.99, wspace=0.01, hspace=0.01)
 
     log.debug('found %d images', nImages)
-    for imageName, image in images.items():
+    for index, (imageName, image) in enumerate(images.items()):
         if image is not None:
             grayimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             ret, corners = cv2.findChessboardCorners(grayimage, (N_COLS, N_ROWS), None)
@@ -91,6 +90,7 @@ def detect_chess_board_corners(images, show=False):
 
     return imagePoints
 
+
 def calibrate_camera(imagePath, show=False):
     images = load_images(imagePath)
     imagePoints = detect_chess_board_corners(images, show=show)
@@ -98,17 +98,21 @@ def calibrate_camera(imagePath, show=False):
     objectPoints = _generate_object_points(nImages=len(imagePointsValues))
     assert(imagePointsValues.shape[:-2] == objectPoints.shape[:-2])
     imageSize = list(images.values())[0].shape[:2]
-    retvalue, KK, Kc , rvecs, tvecs = cv2.calibrateCamera(objectPoints=objectPoints,
-                                                          imagePoints=imagePointsValues,
-                                                          imageSize=imageSize,
-                                                          cameraMatrix=None,
-                                                          distCoeffs=None)
+
+    retvalue, KK, Kc, rvecs, tvecs = cv2.calibrateCamera(
+        objectPoints=objectPoints,
+        imagePoints=imagePointsValues,
+        imageSize=imageSize,
+        cameraMatrix=None,
+        distCoeffs=None
+    )
+
     return KK, Kc, rvecs, tvecs
+
 
 def get_camera_transforms(rvecs, tvecs):
     assert(len(rvecs) == len(tvecs))
     transforms = []
-
     for index in range(len(rvecs)):
         transform = np.eye(4)
         r = R.from_rotvec(rvecs[index].flatten())
@@ -116,6 +120,7 @@ def get_camera_transforms(rvecs, tvecs):
         transform[:3, 3] = tvecs[index].flatten()
         transforms.append(transform)
     return np.array(transforms, copy=False)
+
 
 def undistort_images(images, KK, Kc, show=False):
     undistortedImages = []
@@ -126,6 +131,7 @@ def undistort_images(images, KK, Kc, show=False):
             plt.imshow(undistortedImage)
             plt.pause(0.3)
     return undistortedImages
+
 
 if __name__ == '__main__':
     sciptPath = os.path.dirname(os.path.realpath(__file__))
