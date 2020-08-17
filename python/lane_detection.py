bb4d34d0a9aec6e7b8d2cef0ab915a013d5b963c
Kun Lin <kun.lin.596@gmail.com>
2020-08-17 22:32:56 +0900

    (feat) Implement search_lane in lane_detection

--- python/lane_detection.py
+++ python/lane_detection.py
@@ -6,11 +6,14 @@ import os
 import numpy as np
 
 import matplotlib.pyplot as plt
+
 from IPython import embed
 import matplotlib
 import logging
 
 np.set_printoptions(suppress=True, precision=5)
+plt.ion()
+
 try:
     import colorlog
     handler = colorlog.StreamHandler()
@@ -24,22 +27,24 @@ except ImportError as e:
 
 matplotlib.use('TkAgg')
 
+# Hard coded for test images, these values are tweaked using test_images/straight_lines1.jpg
+IMAGE_SHAPE = (720, 1280)
+ROI_CORNERS = np.float32([[IMAGE_SHAPE[1] / 2 - 62, IMAGE_SHAPE[0] / 2 + 100],
+                          [IMAGE_SHAPE[1] / 6 - 17, IMAGE_SHAPE[0]],
+                          [IMAGE_SHAPE[1] * 5 / 6 + 67, IMAGE_SHAPE[0]],
+                          [IMAGE_SHAPE[1] / 2 + 62, IMAGE_SHAPE[0] / 2 + 100]])
 
-def get_homography(image):
-    imgShape = image.shape
 
-    src = np.float32([[(imgShape[1] / 2) - 55, imgShape[0] / 2 + 100],
-                      [((imgShape[1] / 6) - 10), imgShape[0]],
-                      [(imgShape[1] * 5 / 6) + 60, imgShape[0]],
-                      [(imgShape[1] / 2 + 55), imgShape[0] / 2 + 100]])
+WARPED_ROI_CORNERS = np.float32([[IMAGE_SHAPE[1] / 4, 0],
+                                [IMAGE_SHAPE[1] / 4, IMAGE_SHAPE[0]],
+                                [IMAGE_SHAPE[1] * 3 / 4, IMAGE_SHAPE[0]],
+                                [IMAGE_SHAPE[1] * 3 / 4, 0]])
 
-    dst = np.float32([[(imgShape[1] / 4), 0],
-                      [(imgShape[1] / 4), imgShape[0]],
-                      [(imgShape[1] * 3 / 4), imgShape[0]],
-                      [(imgShape[1] * 3 / 4), 0]])
 
-    roiMaskX = dst[::2, 0].astype(np.int32)
-    return cv2.findHomography(src, dst), src, dst, roiMaskX
+def get_homography():
+    src = ROI_CORNERS
+    dst = WARPED_ROI_CORNERS
+    return cv2.findHomography(src, dst)
 
 
 def load_images(imagePath):
@@ -57,62 +62,63 @@ def load_images(imagePath):
     return images
 
 
-def search_lane(image, leftX, rightX, windowSize=None, show=False):
+def search_lane(image, windowSize=None, show=False):
     if windowSize is None:
-        windowSize = (200, 20)
+        windowSize = (100, 20)
+
+    leftX = WARPED_ROI_CORNERS[0, 0]
+    rightX = WARPED_ROI_CORNERS[2, 0]
 
     windowHeight = windowSize[1]
-    currentY = image.shape[0]
+    currentY = image.shape[0] - windowHeight / 2
     currentLeftX = int(leftX)
     currentRightX = int(rightX)
 
-    halfWindowWidth = windowSize[0] // 2
-
-    # embed()
-
-    def get_window_patch(currentX, currentY):
-        minX = max(currentX - halfWindowWidth, 0)
-        minY = max(currentY - windowHeight, 0)
-        maxX = min(currentX + halfWindowWidth, image.shape[1])
-        maxY = currentY
-
-        patch = image[minY: currentY, minX: maxX]
-
-        topLeftCorner = np.array([minX, minY])
-        bottomRightCorner = np.array([maxX, maxY])
-        return patch, topLeftCorner, bottomRightCorner
+    def get_window_patch(currentX, currentY, windowSize):
+        corners = cv2.boxPoints(((currentX, currentY), windowSize, 0)).astype(np.int32)
+        minXY = corners.min(axis=0)
+        maxXY = corners.max(axis=0)
+        patch = image[minXY[1]: maxXY[1], minXY[0]: maxXY[0]]
+        return patch, minXY, maxXY, corners
 
     leftCenters = []
     rightCenters = []
 
+    if show:
+        plt.cla()
+        plt.imshow(image, cmap='gray')
+        plt.plot(WARPED_ROI_CORNERS[:, 0], WARPED_ROI_CORNERS[:, 1], 'r.')
+        plt.show(block=False)
+
+    currentWindowSize = (200, 20)
+
     while currentY > 0:
-        leftPatch, topLeftCornerLeft, bottomRightCornerLeft = get_window_patch(currentLeftX, currentY)
-        rightPatch, topLeftCornerRight, bottomRightCornerRight = get_window_patch(currentRightX, currentY)
+        leftPatch, topLeftCornerLeft, bottomRightCornerLeft, leftCorners = get_window_patch(currentLeftX, currentY, currentWindowSize)
+        rightPatch, topLeftCornerRight, bottomRightCornerRight, rightCorners = get_window_patch(currentRightX, currentY, currentWindowSize)
 
         validX = np.nonzero(leftPatch)[1]
-        x = sorted(validX)[len(validX) // 2] if len(validX) > 200 else np.nan
+        x = sorted(validX)[len(validX) // 2] if len(validX) > 5 else np.nan
         leftCenter = np.array([x, leftPatch.shape[0] / 2]) + topLeftCornerLeft
 
         validX = np.nonzero(rightPatch)[1]
-        x = sorted(validX)[len(validX) // 2] if len(validX) > 200 else np.nan
+        x = sorted(validX)[len(validX) // 2] if len(validX) > 5 else np.nan
         rightCenter = np.array([x, rightPatch.shape[0] / 2]) + topLeftCornerRight
 
         leftCenters.append(leftCenter)
         rightCenters.append(rightCenter)
 
+        if ~np.isnan(leftCenter[0]):
+            currentLeftX = int(leftCenter[0] + 0.5)
+
+        if ~np.isnan(rightCenter[0]):
+            currentRightX = int(rightCenter[0] + 0.5)
+
         # Move window up
         currentY -= windowSize[1]
-        currentLeftX -= 3
-        currentRightX -= 3
 
-        # if True:
-        #     plt.subplot(131)
-        #     plt.imshow(image, cmap='gray')
-        #     plt.subplot(132)
-        #     plt.imshow(leftPatch)
-        #     plt.subplot(133)
-        #     plt.imshow(rightPatch)
-        # embed()
+        if show:
+            plt.plot(leftCorners[:, 0][[0, 1, 2, 3, 0]], leftCorners[:, 1][[0, 1, 2, 3, 0]], 'r')
+            plt.plot(rightCorners[:, 0][[0, 1, 2, 3, 0]], rightCorners[:, 1][[0, 1, 2, 3, 0]], 'b')
 
     leftCenters = np.array(leftCenters)
     rightCenters = np.array(rightCenters)
@@ -120,20 +126,29 @@ def search_lane(image, leftX, rightX, windowSize=None, show=False):
     leftCenters = leftCenters[~np.isnan(leftCenters[:, 0])]
     rightCenters = rightCenters[~np.isnan(rightCenters[:, 0])]
 
-    leftPoly = np.polyfit(leftCenters[:, 1], leftCenters[:, 0], 2)
-    rightPoly = np.polyfit(rightCenters[:, 1], rightCenters[:, 0], 2)
+    leftPoly = None
+    estimatedLeftX = None
+    if len(leftCenters):
+        leftPoly = np.polyfit(leftCenters[:, 1], leftCenters[:, 0], 2)
+        estimatedLeftX = np.polyval(leftPoly, np.arange(0, image.shape[0]))
 
-    estimatedLeftX = np.polyval(leftPoly, leftCenters[:, 1])
-    estimatedRightX = np.polyval(rightPoly, rightCenters[:, 1])
+    rightPoly = None
+    estimatedRightX = None
+    if len(rightCenters):
+        rightPoly = np.polyfit(rightCenters[:, 1], rightCenters[:, 0], 2)
+        estimatedRightX = np.polyval(rightPoly, np.arange(0, image.shape[0]))
 
     if show:
-        plt.imshow(image, cmap='gray')
         plt.plot(leftCenters[:, 0], leftCenters[:, 1], 'r.')
-        plt.plot(estimatedLeftX, leftCenters[:, 1])
+        if estimatedLeftX is not None:
+            plt.plot(estimatedLeftX, np.arange(0, image.shape[0]))
         plt.plot(rightCenters[:, 0], rightCenters[:, 1], 'g.')
-        plt.plot(estimatedRightX, rightCenters[:, 1])
+        if estimatedRightX is not None:
+            plt.plot(estimatedRightX, np.arange(0, image.shape[0]))
+        plt.pause(0.001)
+        plt.show(block=False)
 
-    embed()
+    return leftPoly, rightPoly
 
 
 def lane_detection(imageName, image, KK, Kc, show=False):
@@ -149,7 +164,6 @@ def lane_detection(imageName, image, KK, Kc, show=False):
             plt.subplot(2, 3, i + 1 + 3)
             plt.imshow(hsvImage[:, :, i], cmap='gray')
 
-    show = False
     # Use S channel in HSL image since the lane color is more out-standing than others
     laneImage = hlsImage[:, :, 2]
 
@@ -162,40 +176,74 @@ def lane_detection(imageName, image, KK, Kc, show=False):
     gradY = np.abs(cv2.Sobel(undistortedImage, cv2.CV_16S, 0, 1, kernelSize))
     sobelImage = cv2.addWeighted(gradX, 0.5, gradY, 0.5, 0)
 
-    lowerBound = np.clip(np.percentile(sobelImage.flatten(), 99), 100, 255)
-    cannyImage = cv2.Canny(sobelImage.astype(np.uint8), lowerBound, 255)
+    cannyImage = cv2.Canny(laneImage.astype(np.uint8), 155, 255)
 
     # Warp image
-    homography, srcCorners, dstCorners, roiMaskX = get_homography(image)
+    homography = get_homography()
     warpTransform = homography[0]
-    warpedImage = cv2.warpPerspective(cannyImage, warpTransform, (cannyImage.shape[1], cannyImage.shape[0]))
-
-    # Optional masking
-    # mask = np.zeros_like(warpedImage, dtype=np.bool)
-    # mask[:, roiMaskX[0]: roiMaskX[1]] = True
-    # warpedImage[~mask] = 0
-
-    # Sliding window lane searching
-    search_lane(warpedImage, leftX=roiMaskX[0], rightX=roiMaskX[1])
+    warpedCannyImage = cv2.warpPerspective(cannyImage, warpTransform, (cannyImage.shape[1], cannyImage.shape[0]))
+    warpedImage = cv2.warpPerspective(image, warpTransform, (cannyImage.shape[1], cannyImage.shape[0]))
 
     if show:
-        plt.subplot(121)
+        plt.figure()
+        plt.ion()
+        plt.subplot(321)
+        plt.imshow(image)
+        plt.plot(ROI_CORNERS[:, 0], ROI_CORNERS[:, 1], 'r.')
+        plt.subplot(322)
+        plt.imshow(warpedImage)
+        plt.plot(WARPED_ROI_CORNERS[:, 0], WARPED_ROI_CORNERS[:, 1], 'r.')
+        plt.subplot(323)
         plt.imshow(cannyImage, cmap='gray')
-        plt.subplot(122)
-        plt.imshow(warpedImage, cmap='gray')
+        plt.subplot(324)
+        plt.imshow(warpedCannyImage, cmap='gray')
+        plt.subplot(325)
+        plt.imshow(sobelImage, cmap='gray')
+        plt.subplot(326)
+        plt.imshow(cannyImage, cmap='gray')
+        plt.pause(0.001)
+        plt.show(block=False)
 
-    embed()
+    # Sliding window lane searching
+    leftPoly, rightPoly = search_lane(warpedCannyImage)
+
+    yRange = np.arange(0, warpedImage.shape[0])
+    if leftPoly is not None:
+        leftX = np.polyval(leftPoly, yRange)
+    if rightPoly is not None:
+        rightX = np.polyval(rightPoly, yRange)
+
+    warpedLeftLaneCorners = np.vstack([leftX, yRange]).T
+    warpedRightLaneCorners = np.vstack([rightX, yRange]).T
+
+    warpTransformInv = np.linalg.inv(warpTransform)
+    leftLaneCorners = cv2.perspectiveTransform(warpedLeftLaneCorners.reshape(-1, 1, 2), warpTransformInv).reshape(-1, 2)
+    rightLaneCorners = cv2.perspectiveTransform(warpedRightLaneCorners.reshape(-1, 1, 2), warpTransformInv).reshape(-1, 2)
+
+    detectionOverlap = np.zeros_like(image)
+    detectionOverlap = cv2.fillPoly(detectionOverlap, np.vstack([leftLaneCorners, rightLaneCorners[::-1]]).astype(np.int32).reshape(1, -1, 2), color=[0, 255, 0])
+    detectionOverlap = cv2.polylines(detectionOverlap, leftLaneCorners.astype(np.int32).reshape(1, -1, 2), False, color=[255, 0, 0], thickness=3)
+    detectionOverlap = cv2.polylines(detectionOverlap, rightLaneCorners.astype(np.int32).reshape(1, -1, 2), False, color=[0, 0, 255], thickness=3)
+
+    overlayImage = cv2.addWeighted(image, 1.0, detectionOverlap, 0.5, 0.0)
+
+    if show:
+        plt.clf()
+        plt.imshow(overlayImage)
+        # plt.plot(leftLaneCorners[:, 0], leftLaneCorners[:, 1], 'r')
+        # plt.plot(rightLaneCorners[:, 0], rightLaneCorners[:, 1], 'b')
+        plt.pause(0.001)
+        plt.show(block=False)
+        embed()
 
 
 def lane_detections(images, KK, Kc, show=False):
     for index, (imageName, image) in enumerate(images.items()):
         lane_detection(imageName, image, KK, Kc)
-    embed()
 
 
 if __name__ == '__main__':
     plt.ion()
-
     sciptPath = os.path.dirname(os.path.realpath(__file__))
     testImagePath = os.path.join(sciptPath, '..', 'test_images')
     images = load_images(testImagePath)
